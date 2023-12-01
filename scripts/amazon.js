038a90a761a7e6f719bc4ed5130e211f2345447d
Zhanserik <k.zhanserik@gmail.com>
2023-12-01 17:21:10 +0500

    moved addToCart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js"
+import {cart,addToCart} from "../data/cart.js"
 
 import {products} from "../data/products.js"
 
@@ -61,30 +61,11 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function addToCart(productId){
-  let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      if (matchingItem) {
-        matchingItem.quantity += 1;
-      } else {
-        cart.push({
-          productId: productId,
-          quantity: 1
-        });
-      }
-}
-
 function updateCartQuantity(){
   let cartQuantity = 0;
 
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
+      cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
       });
 
       document.querySelector('.js-cart-quantity')
